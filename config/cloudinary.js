d07dd180443ecc49507c369cab2705bfc75303b1
A-loxxha <charleskush77@gmail.com>
2025-06-18 06:45:18 +0300

    Update cloudinary.js config

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,11 +1,5 @@
 const cloudinary = require('cloudinary').v2;
 
-console.log('Cloudinary ENV check:', {
-  cloud: process.env.CLOUDINARY_CLOUD_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secret: process.env.CLOUDINARY_API_SECRET
-});
-
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
