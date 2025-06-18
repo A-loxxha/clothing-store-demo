7c8f9e3779a835b3e385eca21a23c51e256f3089
A-loxxha <charleskush77@gmail.com>
2025-06-18 19:06:45 +0300

    upadate cookies

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 require('dotenv').config();
 const express  = require('express');
 const mongoose = require('mongoose');
-const cors     = require('cors');
+const cors = require('cors');
+
 const path     = require('path');
 const { initiatePayment } = require('./pesapal');
 
