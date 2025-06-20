c5bd1f19f83576712d4de4e8bee9d836f8306349
A-loxxha <charleskush77@gmail.com>
2025-06-20 05:26:21 +0300

    email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -9,13 +9,21 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendVerificationEmail = async (to, token) => {
-  const verifyUrl = `${process.env.CLIENT_URL}/api/users/verify/${token}`;
+  const verifyUrl = `${process.env.SERVER_URL}/api/users/verify/${token}`;
+
 
   await transporter.sendMail({
     from: `"Your Store" <${process.env.EMAIL_USER}>`,
     to,
     subject: 'Verify your email',
-    html: `<h3>Verify your account</h3><p>Click the link below:</p><a href="${verifyUrl}">Verify Email</a>`
+   html: `
+  <h3>Verify your account</h3>
+  <p>Thanks for signing up with Mabel Statement. Click the button below to verify your email address:</p>
+  <p><a href="${verifyUrl}" style="background:#ceb974; color:white; padding:10px 20px; text-decoration:none; border-radius:5px;">Verify Email</a></p>
+  <p>If the button doesn't work, copy and paste this link into your browser:</p>
+  <p>${verifyUrl}</p>
+`
+
   });
 };
 
